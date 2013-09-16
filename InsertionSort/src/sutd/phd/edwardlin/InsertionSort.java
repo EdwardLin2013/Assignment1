b23adc4a8ee29d8193356ec0f93a1cca93bfe4c8
Kin Wah Edward Lin <edward_lin@mymail.sutd.edu.sg>
2013-09-16 15:55:37 +0800

    Github Testing with eclipse

---
+++ InsertionSort/src/sutd/phd/edwardlin/InsertionSort.java
@@ -0,0 +1,11 @@
+package sutd.phd.edwardlin;
+
+public class InsertionSort 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
